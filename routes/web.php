124b78a77eebc12e6db104db76cad019bcc67ac7
Rodrigo Ferrer <admin@tecsisa.com>
2026-03-01 15:04:44 -0500

    refactor: completely remove the port management wizard and related connections APIs per contractor change of plans

--- routes/web.php
+++ routes/web.php
@@ -52,9 +52,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
             Route::get('/racks', [\App\Http\Controllers\RackBuilderController::class , 'index'])->name('rack.builder');
             Route::post('/racks/{rack}/save', [\App\Http\Controllers\RackBuilderController::class , 'save'])->name('rack.save');
-            Route::get('/api/equipment/{equipment}/ports', [\App\Http\Controllers\RackBuilderController::class , 'getEquipmentPorts'])->name('api.equipment.ports');
-            Route::post('/api/connections', [\App\Http\Controllers\RackBuilderController::class , 'connectPorts'])->name('api.connections.store');
-            Route::delete('/api/connections/{connection}', [\App\Http\Controllers\RackBuilderController::class , 'disconnectConnection'])->name('api.connections.destroy');
         }
         );
 
