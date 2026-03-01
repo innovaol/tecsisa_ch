b8a446911a30f3330b27cee56d5371d8a45984a1
Rodrigo Ferrer <admin@tecsisa.com>
2026-03-01 15:16:44 -0500

    chore: enable rack seeder directly inside databaseseeder to populate dummy racks along with equipment on migrate:fresh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class ,
             SystemSeeder::class ,
             EquipmentSeeder::class ,
+            RackSeeder::class ,
         ]);
     }
 }
