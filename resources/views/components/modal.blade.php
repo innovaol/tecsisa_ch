a9d4f1aadd47b18051e0a6d00dc83eb3e88a4826
Rodrigo Ferrer <admin@tecsisa.com>
2026-03-01 02:18:43 -0500

    Fase 6.3.1: Hotfix de Modal - Añadido soporte para anchos 4xl y 7xl en el componente base de modal para evitar error de llave indefinida.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -11,6 +11,8 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
+    '4xl' => 'sm:max-w-4xl',
+    '7xl' => 'sm:max-w-7xl',
 ][$maxWidth];
 @endphp
 
